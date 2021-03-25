7134d0b5bdce49105644d6a7cb9bb166268088b5
Rafael Tumasian <r.tumasyan@gmail.com>
2021-03-25 21:34:53 +0400

    initial commit

---
+++ src/main/java/com/example/analizegithubusers/AnalizeGithubUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.analizegithubusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalizeGithubUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalizeGithubUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/analizegithubusers/AnalizeGithubUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.analizegithubusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalizeGithubUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
